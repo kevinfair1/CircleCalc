43ab1574915830b3786c3dedcbdcbbed6e703a5f
kevinfair1 <70407795+kevinfair1@users.noreply.github.com>
2020-08-28 16:25:10 -0400

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CsharpWeek11CircleCalc
+{
+    public class Circle
+    {
+        private double radius;
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+                if (radius < 0)
+                    radius = radius * -1;
+            }
+        }
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double getArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public double getCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+
+}
---
+++ MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+
+namespace CsharpWeek11CircleCalc
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            TextView tvResult = FindViewById<TextView>(Resource.Id.textView1);
+            EditText etRadius = FindViewById<EditText>(Resource.Id.editText1);
+
+            button.Click += delegate
+            {
+                //button.Text = string.Format("{0} clicks!", count++)
+                Circle myCircle = new Circle(Double.Parse(etRadius.Text));
+                tvResult.Text = "Area: " + myCircle.getArea() +
+                                "\nCircumference: " + myCircle.getCircumference();
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
